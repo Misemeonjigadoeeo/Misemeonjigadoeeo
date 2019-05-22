25a70f345be691f8e79c6d5a3f19481f641b3ec6
이진우 <binarywoo@gmail.com>
2019-05-22 13:49:50 +0900

    위치 데이터 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,25 +31,37 @@ RefreshController _refreshController = RefreshController();
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var currentTime = new DateTime.now();
+  var location = new Location();
+  LocationData userLocation;
+  bool locationPermission = false;
 
   void initState() {
     super.initState();
     _refreshController = RefreshController();
+    setLocation();
   }
 
-  void _onRefresh(){
-
+  void _onRefresh() {
     /*.  after the data return,
           use _refreshController.refreshComplete() or refreshFailed() to end refreshing
     */
     _refreshController.refreshFailed();
+    setLocation();
   }
 
-  void _onLoading(){
+  void _onLoading() {
     /*
           use _refreshController.loadComplete() or loadNoData() to end loading
     */
     _refreshController.loadNoData();
+  }
+
+  void _permissionChange(bool value) {
+    setState(() {
+      locationPermission = value;
+    });
+    setLocation();
   }
 
   void _incrementCounter() {
@@ -64,7 +78,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: appbar,
       body: Container(
-        color: Colors.blue,
         child: SmartRefresher(
           enablePullDown: true,
           header: WaterDropHeader(),
@@ -75,7 +88,6 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
                   Text(
                     'You have pushed the button this many times:',
@@ -84,6 +96,17 @@ class _MyHomePageState extends State<MyHomePage> {
                     '$_counter',
                     style: Theme.of(context).textTheme.display1,
                   ),
+                  Text('현재 시간 - $currentTime'),
+                  Text(locationPermission && userLocation != null
+                      ? '현재 위치 - ${userLocation.latitude}, ${userLocation.longitude}'
+                      : '위치 권한 없음'),
+                  SwitchListTile(
+                      value: locationPermission,
+                      onChanged: _permissionChange,
+                      title: Text('위치 권한'))
+                  // 위도 - userLocation.latitude
+                  // 경도 - userLocation.longitude
+                  // 고도 - userLocation.altitude
                 ],
               )
             ],
@@ -98,7 +121,46 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void dispose(){
+  setLocation() async {
+    await location.changeSettings(
+        accuracy: LocationAccuracy.HIGH, interval: 1000);
+    LocationData currentLocation;
+
+    try {
+      bool status = await location.serviceEnabled();
+      bool permission = false;
+
+      print('Service status: $status');
+      if (status) {
+        permission = await location.requestPermission();
+        print('Permission: $permission');
+        if (permission) {
+          currentLocation = await location.getLocation();
+        }
+      } else {
+        bool statusResult = await location.requestService();
+        print('Service status activated after request: $statusResult');
+        if (statusResult) {
+          setLocation();
+        }
+      }
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        print('asdf');
+        currentLocation = null;
+      }
+    } catch (e) {
+      debugPrint(e);
+      currentLocation = null;
+    }
+
+    setState(() {
+      userLocation = currentLocation;
+      currentTime = new DateTime.now();
+    });
+  }
+
+  void dispose() {
     _refreshController.dispose();
     super.dispose();
   }
