5ab1337b21d3d46ef631a322e9874d86db93c6d4
최혁재 <gurwo1007@gmail.com>
2019-06-01 14:47:13 +0900

    라이브러리 버전 업 및 폴더 분리

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:misemeonjigadoeeo/main.dart';
+import 'package:misemeonjigadoeeo/ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
