8ecf7a0e0ad518c3a5f177bdba47bb5471bfa48a
최혁재 <gurwo1007@gmail.com>
2019-08-10 16:40:12 +0900

    미세먼지 api 토큰값 비공개 클래스에 전달

--- lib/service/fine_dust_service.dart
+++ lib/service/fine_dust_service.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
 import 'package:misemeonjigadoeeo/response/fine_dust_response.dart';
+import 'package:misemeonjigadoeeo/secret/private_key.dart';
 
 class FineDustService {
   String baseUrl="https://api.waqi.info/feed/";
 
   Future<FineDustResponse> getFineDustInfoByLatLng(Position pos) async {
-    String apiUrl = baseUrl + "geo:" + pos.latitude.toString() + ";" + pos.longitude.toString() + "/?token=" + "9be421958a0c7b7546c3a30bbda4d2e717d6bf22";
+    String apiUrl = baseUrl + "geo:" + pos.latitude.toString() + ";" + pos.longitude.toString() + "/?token=" + PrivateKey.waqiApiKey;
     final response = await http.get(apiUrl);
 
     if(response.statusCode == 200) {
