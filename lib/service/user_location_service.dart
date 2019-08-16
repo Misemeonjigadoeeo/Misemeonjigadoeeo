7313f39ec0587c5367295bd76d22079989b5cadb
최혁재 <gurwo1007@gmail.com>
2019-08-16 16:40:11 +0900

    카카오 위경도 -> 주소 변환 api 호출 함수 추가

--- lib/service/user_location_service.dart
+++ lib/service/user_location_service.dart
@@ -1,4 +1,9 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:misemeonjigadoeeo/response/kakao_local_api_response.dart';
+import 'package:misemeonjigadoeeo/secret/private_key.dart';
 
 import 'package:geolocator/geolocator.dart';
 
@@ -9,6 +14,21 @@ class UserLocationService {
 
     if(isLocationAvailable) {
       return Geolocator().getLastKnownPosition();
+    }
+    else {
+      return null;
+    }
+  }
+
+  Future<KakaoLocalApiResponse> getAddressFromPosition(Position position) async {
+    String kakaoLocalApiBaseUrl = "https://dapi.kakao.com/v2/local/geo/coord2address.json?";
+    final addressResponse = await http.get(
+      kakaoLocalApiBaseUrl + "x=" + position.longitude.toString() + "&y=" + position.latitude.toString() + "&input_coord=WGS84",
+      headers: {HttpHeaders.authorizationHeader: "KakaoAK ${PrivateKey.kakaoRestApiKey}"}
+    );
+
+    if(addressResponse.statusCode == 200) {
+      return KakaoLocalApiResponse.fromJson(jsonDecode(addressResponse.body));
     }
     else {
       return null;
